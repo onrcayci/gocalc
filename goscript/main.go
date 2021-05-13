c67f022446fc76b5def36f3b58bd1d3f8932b0d5
Onur Cayci <onur.cayci@outlook.com>
2021-05-13 15:58:46 -0400

    Implement parseContent to tokenize the file content; Update main function to read a text file and parse it contents

--- goscript/main.go
+++ goscript/main.go
@@ -1,13 +1,47 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+
+	"github.com/onrcayci/goscript/parser"
 )
 
 func main() {
 
 	// get the command line arguments
-	args := os.Args[1:]
-	fmt.Println(args)
+	filename := os.Args[1]
+
+	// open the file for reading
+	file, err := os.Open(filename)
+
+	// check for errors
+	if err != nil {
+		panic(err)
+	}
+
+	// close file with defer
+	defer file.Close()
+
+	// initialize scanner to read the file line by line
+	scanner := bufio.NewScanner(file)
+
+	// initialize the array to store the lines
+	var lines string
+
+	// loop over the file and scan each line
+	for scanner.Scan() {
+		lines += scanner.Text()
+	}
+
+	// check for read errors
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s\n", lines)
+
+	// parse the contents of the file
+	parser.ParseContent(lines)
 }
--- parser/parser.go
+++ parser/parser.go
@@ -1,1 +1,21 @@
 package parser
+
+import (
+	"fmt"
+	"strings"
+	"text/scanner"
+)
+
+func ParseContent(fileContents string) {
+
+	// initialize the scanner
+	var scan scanner.Scanner
+	scan.Init(strings.NewReader(fileContents))
+
+	i := 0
+
+	for token := scan.Scan(); token != scanner.EOF; token = scan.Scan() {
+		fmt.Printf("token %d: %s \n", i, scan.TokenText())
+		i++
+	}
+}
