ef49f2885998b3a9507d446cf5294b1310742273
Onur Cayci <onur.cayci@mail.mcgill.ca>
2021-05-14 14:52:25 -0400

    Implement function to parse infix expression to postfix

--- utils/utils.go
+++ utils/utils.go
@@ -2,8 +2,11 @@ package utils
 
 import (
 	"bufio"
+	"container/list"
 	"fmt"
 	"os"
+
+	"github.com/onrcayci/gocalculator/grammar"
 )
 
 func HandleError(err error) {
@@ -19,11 +22,50 @@ func GetInput() string {
 	return input
 }
 
+func InfixToPostFix(tokens []string) []string {
+	var postfix []string
+	stack := list.New()
+	for i := 0; i < len(tokens); i++ {
+		currentToken := tokens[i]
+		if !grammar.IsOperator(currentToken) {
+			postfix = append(postfix, currentToken)
+		} else if currentToken == "(" {
+			stack.PushFront(currentToken)
+		} else if currentToken == ")" {
+			for stack.Len() != 0 && stack.Front().Value.(string) != "(" {
+				top := stack.Front()
+				stack.Remove(top)
+				postfix = append(postfix, top.Value.(string))
+			}
+			lpar := stack.Front()
+			stack.Remove(lpar)
+		} else {
+			for stack.Len() != 0 && grammar.GetOperatorPrecedence(currentToken) <= grammar.GetOperatorPrecedence(stack.Front().Value.(string)) {
+				top := stack.Front()
+				stack.Remove(top)
+				postfix = append(postfix, top.Value.(string))
+			}
+			stack.PushFront(currentToken)
+		}
+	}
+
+	// pop all remaining elements from the stack
+	for stack.Len() != 0 {
+		top := stack.Front()
+		stack.Remove(top)
+		postfix = append(postfix, top.Value.(string))
+	}
+
+	return postfix
+}
+
 func ExecuteInput(tokens []string) {
 	switch tokens[0] {
 	case "exit":
 		os.Exit(0)
 	default:
-		fmt.Println("Unknown Input!")
+		fmt.Println(tokens)
+		postfix := InfixToPostFix(tokens)
+		fmt.Println(postfix)
 	}
 }
