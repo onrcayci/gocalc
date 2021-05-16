ed50a255c70e0620d445dce9365fc6a8ee3a28d2
Onur Cayci <onur.cayci@outlook.com>
2021-05-16 15:37:08 -0400

    Finish implementing grammar module unit tests

--- grammar/grammar_test.go
+++ grammar/grammar_test.go
@@ -21,14 +21,44 @@ func TestIsOperator_ReturnsFalse(t *testing.T) {
 	for i := 0; i < len(operands); i++ {
 		result := grammar.IsOperator(operands[i])
 		if result {
-			t.Errorf("Fail to detect operand %s as an operand!\n", operands[i]);
+			t.Errorf("Fail to detect operand %s as an operand!\n", operands[i])
 		}
 	}
 }
 
 func TestGetOperatorPrecedence_Power(t *testing.T) {
-	precendence := grammar.GetOperatorPrecedence("^")
-	if precendence != 3 {
-		t.Errorf("Fail to detect precedence of '^'\nExpected: 3\nOutcome: %T\n", precendence)
+	precedence := grammar.GetOperatorPrecedence("^")
+	if precedence != 3 {
+		t.Errorf("Fail to detect precedence of '^'\nExpected: 3\nOutcome: %d\n", precedence)
+	}
+}
+
+func TestGetOperatorPrecedence_MultiplyDivide(t *testing.T) {
+	operators := [2]string{"*", "/"}
+	for i := 0; i < len(operators); i++ {
+		precedence := grammar.GetOperatorPrecedence(operators[i])
+		if precedence != 2 {
+			t.Errorf("Fail to detect precedence of %s\nExpected: 2\nOutcome: %d\n", operators[i], precedence)
+		}
+	}
+}
+
+func TestGetOperatorPrecedence_PlusMinus(t *testing.T) {
+	operators := [2]string{"+", "-"}
+	for i := 0; i < len(operators); i++ {
+		precedence := grammar.GetOperatorPrecedence(operators[i])
+		if precedence != 1 {
+			t.Errorf("Fail to detect precedence of %s\nExpected: 1\nOutcome: %d\n", operators[i], precedence)
+		}
+	}
+}
+
+func TestGetOperatorPrecedence_OperandsParantheses(t *testing.T) {
+	characters := [3]string{"12", "(", ")"}
+	for i := 0; i < len(characters); i++ {
+		precedence := grammar.GetOperatorPrecedence(characters[i])
+		if precedence != 0 {
+			t.Errorf("Fail to detect precedence of %s\nExpected: 0\nOutcome: %d\n", characters[i], precedence)
+		}
 	}
 }
